5bbccea645eb139963757dcf243f32706ca9a6c3
john L <johnleavell@gmail.com>
2019-11-30 03:16:49 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -4,10 +4,13 @@ class CashRegister
   attr_accessor :discount
 
   def initialize(discount = 0)
-    @total = 0
-    @discount = discount
+    @total = 0 #sets an instance variable @total on initialization to zero
+    @discount = discount #optionally takes an employee discount on intialization
   end
 
+  def total
+    @total
+  end
 
 
 
